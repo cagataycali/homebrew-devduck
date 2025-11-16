4f146595eeb363c2724e2a34eb8675f11846c29b
cagataycali <cagataycali@icloud.com>
2025-11-16 01:25:22 -0500

    Install from PyPI directly to get all dependencies automatically

--- Formula/devduck.rb
+++ Formula/devduck.rb
@@ -9,13 +9,12 @@ class Devduck < Formula
 
   depends_on "python@3.13"
 
-  resource "devduck" do
-    url "https://files.pythonhosted.org/packages/source/d/devduck/devduck-0.5.2.tar.gz"
-    sha256 "bb43f99a0310da96b081ec187ef9412c3254c4fed48dc3e88578de6b31b6e570"
-  end
-
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.13")
+    # Install directly from PyPI to get all dependencies
+    system libexec/"bin/pip", "install", "--upgrade", "pip"
+    system libexec/"bin/pip", "install", "devduck==0.5.2"
+    bin.install_symlink libexec/"bin/devduck"
   end
 
   test do
